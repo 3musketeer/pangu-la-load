2274afd8d645c546cb41169cfa3d66fb60a62ccd
wanzhou <wanzhou@asiainfo.com>
2015-05-21 18:12:06 +0800

    update 3G ESS group

--- app/controllers/Alarm3GESSPlugin.js
+++ app/controllers/Alarm3GESSPlugin.js
@@ -49,6 +49,30 @@ exports.Alarm3GESSLoader = function(data, host){
 	var engine = new LaEngine();
 	engine.add(Alarm3GESSParser(host)) //解析字串
 		.add(engine.save("YYYYMMDD"))    //分主机按天保存
-		.add(engine.showError())//显示错误
+        .add(engine.group("Group",function(data){
+            var count = {};
+            count[data['rspcode']] = 1;
+            return {"$inc": count}
+        },{
+            group1 : function(data){
+                return {'host': data.host, 'type': 'code', 'servicename': data.servicename}
+            },
+            group2 : function(data){
+                return {'host': data.host, 'type': 'code', 'operatename': data.operatename}
+            }
+        },"day"))
+/*        .add(engine.group("Group", function(data){
+            var count = {};
+            count[data['rspdesc']] = 1;
+            return {"$inc": count}
+        },{
+            group1 : function(data){
+                return {'host': data.host, 'type': 'desc', 'servicename': data.servicename}
+            },
+            group2 : function(data){
+                return {'host': data.host, 'type': 'desc', 'operatename': data.operatename}
+            }
+        },"day"))
+*/		.add(engine.showError())//显示错误
 		.run(data,"Alarm3GESS");
 }
